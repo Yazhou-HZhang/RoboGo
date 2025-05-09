cc44553388eb0c1b119913ed198242dbe7856c8e
Yicheng Wang <wycheng@stanford.edu>
2025-05-09 15:56:01 -0700

    Add InspectionPose to test.py for table overview

--- tidybot2-main/test.py
+++ tidybot2-main/test.py
@@ -3,35 +3,48 @@ import numpy as np
 from mujoco_env import MujocoEnv
 
 def main():
-    env = MujocoEnv()
+    env = MujocoEnv(show_images=True)
     env.reset()
 
-    action1 = {
-        'arm_pos': np.array([0.8, 0, 0.5]),
-        'arm_quat': np.array([0, 0, 0, 1]),
-        'gripper_pos': np.array([1.0]),
-        'base_pose': np.array([-0.2, -0.2, 0.0]),
-    }
 
-
-    action2 = {
-        'arm_pos': np.array([1, 0, 0.5]),
-        'arm_quat': np.array([0.7071, 0, 0, 0.7071]),
-        'gripper_pos': np.array([1.0]),
-        'base_pose': np.array([-0.2, -0.2, 0.0]),
+    InspectionAction = { 
+        'arm_pos': np.array([0.3, 0, 0.7]),
+        'arm_quat': np.array([0.6123724, 0.6123724, 0.3535534, 0.3535534]),
+        'gripper_pos': np.array([0]),
+        'base_pose': np.array([0.0, 0.0, 0.0]), # modify this to the desired base position
     }
 
     # Send command for 30 steps
     for i in range(30):
-        env.step(action1)
+        env.step(InspectionAction)
         time.sleep(0.1)
         print(f"{i + 1}\n")
 
+    # action1 = {
+    #     'arm_pos': np.array([0.8, 0, 0.5]),
+    #     'arm_quat': np.array([0, 0, 0, 1]),
+    #     'gripper_pos': np.array([1.0]),
+    #     'base_pose': np.array([-0.2, -0.2, 0.0]),
+    # }
 
-    for i in range(100):
-        env.step(action2)
-        time.sleep(0.1)
-        print(f"{i + 1}\n")
+    # action2 = {
+    #     'arm_pos': np.array([1, 0, 0.5]),
+    #     'arm_quat': np.array([0.7071, 0, 0, 0.7071]),
+    #     'gripper_pos': np.array([1.0]),
+    #     'base_pose': np.array([-0.2, -0.2, 0.0]),
+    # }
+
+    # # Send command for 30 steps
+    # for i in range(30):
+    #     env.step(action1)
+    #     time.sleep(0.1)
+    #     print(f"{i + 1}\n")
+
+
+    # for i in range(100):
+    #     env.step(action2)
+    #     time.sleep(0.1)
+    #     print(f"{i + 1}\n")
 
 
     # Keep sim alive (optional) until user stops it
